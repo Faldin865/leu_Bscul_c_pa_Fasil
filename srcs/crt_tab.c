7b95e040342f1fa5e753839f5a336e6e805621d1
Theo Sannie <vipers12000@gmail.com>
2020-07-20 19:06:21 +0200

    ++crt_tab.c

---
+++ srcs/crt_tab.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   crt_tab.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/20 17:47:34 by tsannie           #+#    #+#             */
+/*   Updated: 2020/07/20 19:00:12 by tsannie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+
+int		nb_char_file(char *map_file)
+{
+	int		fd;
+	char	buf;
+	int		i;
+
+	i = 0;
+	fd = open(map_file, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	while ((read(fd, &buf, 1)))
+		i++;
+	close(fd);
+	return (i);
+}
+
+char	*imp(char *res, char *map_file)
+{
+	int		fd;
+	char	buf;
+	int		i;
+
+	i = 0;
+	fd = open(map_file, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	while ((read(fd, &buf, 1)))
+	{
+		res[i] = buf;
+		i++;
+	}
+	res[i] = '\0';
+	close(fd);
+	return (res);
+}
+
+char	**ouverture(char *map_file)
+{
+	int		i;
+	char	*str;
+	char	**res;
+
+	i = 0;
+	if (!(str = malloc(sizeof(char) * nb_char_file(map_file) + 1)))
+		return (NULL);
+	imp(str, map_file);
+	res = ft_split(str, "\n");
+	free(str);
+	return (res);
+}
